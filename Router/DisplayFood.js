828957e9a6377ec0b664b58d0b497bc4b280d3fd
Manankumar6 <manankumar2019@gmail.com>
2024-03-04 10:47:40 +0530

    update vercel

--- Router/DisplayFood.js
+++ Router/DisplayFood.js
@@ -5,7 +5,8 @@ const router = express.Router();
 router.get('/foodData', async (req, res) => {
     try {
       
-        res.status(200).send(global.food_items);
+        res.status(200).josn(global.food_items);
+        console.log(global.food_items)
     } catch (error) {
         console.error(error);
         res.status(500).send({ error: 'Internal Server Error' });
