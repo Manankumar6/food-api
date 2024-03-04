80b1255980044a9898721d12cc10fdd997159d56
Manankumar6 <manankumar2019@gmail.com>
2024-03-04 10:45:33 +0530

    update vercel.json

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const cors = require('cors');
 const connectDB = require('./db')
-const PORT = process.env.PORT ||  3000;
+const PORT = process.env.PORT ||  5000;
 
 // let's tackle cors
 const corsOptions = {
