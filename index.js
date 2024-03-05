0c981754ea733cf39b7bef2bad98425664ef7e31
Manankumar6 <manankumar2019@gmail.com>
2024-03-05 10:04:10 +0530

    update cors

--- index.js
+++ index.js
@@ -7,11 +7,12 @@ const PORT = process.env.PORT ||  8080;
 
 // let's tackle cors
 const corsOptions = {
-    origin: 'http://localhost:3000',
-   methods: "GET,POST,PUT,DELETE, PATCH,HEAD",
-   credential:true
+    origin: true,
+    methods: "GET,POST,PUT,DELETE,PATCH,HEAD",
+    credentials: true
   }
-app.use(cors(corsOptions));
+  app.use(cors(corsOptions));
+  
 
 app.use(express.json())
 app.use("/api", require("./Router/User"))
